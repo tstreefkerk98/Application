821bc8bd78fd39fc5088eabd77b93a3276492e76
tstreefkerk <t.s.streefkerk@student.tudelft.nl>
2020-04-06 12:35:44 +0200

    Place project into git repository.

---
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    static final int REQUEST_IMAGE_CAPTURE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        Button takePictureButton = (Button) findViewById(R.id.takePictureButton);
+        takePictureButton.setOnClickListener(takePictureButtonListener);
+
+        if (!Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+        Python pyInstance = Python.getInstance();
+        PyObject pyModule = pyInstance.getModule("ProcessImage");
+        PyObject pyAttribute = pyModule.callAttr("test");
+
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(pyAttribute.toString());
+    }
+
+    View.OnClickListener takePictureButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
